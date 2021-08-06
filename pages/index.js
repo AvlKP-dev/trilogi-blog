3730aed1fa707b4f4fa18b6de055e798ce977567
AvlKP-dev <avlkp.dev@gmail.com>
2021-08-06 23:15:26 +0700

    Added Netlify identity widget

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Head from "next/head";
 import styled from "styled-components";
 
 const Home = () => {
-	return <div></div>;
+	return (
+		<>
+			<Head>
+				<script src="https://identity.netlify.com/v1/netlify-identity-widget.js"></script>
+			</Head>
+		</>
+	);
 };
 
 export default Home;
